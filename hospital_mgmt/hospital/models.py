561240bc3ee6261c1922ab25152e1a856ae0cdd2
Anju Dhungana <anjudhungana28@gmail.com>
2022-03-30 14:56:42 -0400

    create models for doctor

--- hospital_mgmt/hospital/models.py
+++ hospital_mgmt/hospital/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    mobile = models.IntegerField()
+    speciality = models.CharField(max_length=50)
+
+
+    
