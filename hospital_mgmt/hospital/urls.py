5a2d32c1c227e2406f6e7b2008efb5e738ee117e
Anju Dhungana <anjudhungana28@gmail.com>
2022-03-30 16:25:38 -0400

    created login views for admin

--- hospital_mgmt/hospital/urls.py
+++ hospital_mgmt/hospital/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
-from .views import Home, About, Service, Contact
+from .views import Home, About, Service, Contact, Login
 
 urlpatterns = [
     path('',Home,name='home'),
     path('about/',About,name='about'),
     path('service/',Service,name='service'),
     path('contact/',Contact,name='contact'),
+    path('admin_login/',Login,name='login'),
    
 ]
--- hospital_mgmt/hospital/views.py
+++ hospital_mgmt/hospital/views.py
@@ -1,5 +1,8 @@
+from distutils.log import error
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -14,3 +17,31 @@ def Contact(request):
 
 def Service(request):
     return render(request, 'service.html')
+
+def Index(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    return render(request, 'index.html')
+
+
+# login view
+def Login(request):
+    error = ""
+    if request.method=="POST":
+        uname = request.POST["uname"]
+        pwd = request.POST["pwd"]
+        user = authenticate(username= uname, password = pwd )
+        try:
+            if user.is_staff:
+                Login(request,user)
+                error = "No"
+
+            else:
+                error = "yes"
+
+        except:
+            error = "yes"
+            d = {'error': error}
+            return render(request, 'login.html', d)
+
+
