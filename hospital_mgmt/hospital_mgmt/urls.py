2f0848e091146b854a0f92527b72f9a7f698d1bf
Anju Dhungana <anjudhungana28@gmail.com>
2022-03-30 12:24:24 -0400

    created views, urls to check the routes

--- hospital_mgmt/hospital_mgmt/urls.py
+++ hospital_mgmt/hospital_mgmt/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hospital.views import About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',About, name='about')
 ]
