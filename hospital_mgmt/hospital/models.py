8367ed9738be7a75db78deb8ed0da156ffabb97b
Anju Dhungana <anjudhungana28@gmail.com>
2022-03-30 15:06:49 -0400

    added patient model

--- hospital_mgmt/hospital/models.py
+++ hospital_mgmt/hospital/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+from time import time
 from unicodedata import name
 from django.db import models
 
@@ -8,4 +10,10 @@ class Doctor(models.Model):
     speciality = models.CharField(max_length=50)
 
 
-    
+class Patient(models.Model):
+    name = models.CharField(max_length=50)
+    mobile = models.IntegerField(null=True)
+    address = models.TextField()
+
+
+
