af345c74de523d2588bfa0ddb9317666429bc8dd
Anju Dhungana <anjudhungana28@gmail.com>
2022-03-30 14:52:07 -0400

    create pages and added their urls

--- hospital_mgmt/hospital/urls.py
+++ hospital_mgmt/hospital/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from .views import Home, About
+from .views import Home, About, Service, Contact
 
 urlpatterns = [
     path('',Home,name='home'),
     path('about/',About,name='about'),
+    path('service/',Service,name='service'),
+    path('contact/',Contact,name='contact'),
    
 ]
